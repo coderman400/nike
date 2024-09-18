880acaf130f3e0e815f20fe493bea8d5a6470588
arvind <achurox1121@gmail.com>
2024-09-19 01:50:39 +0530

    import more assets, add navbar mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import {Hero, Footer, Offer, CustomerReview, Products, Services, SuperQuality, Subscribe} from './sections'
+import Nav from './components/Nav.jsx'
+
 const App = () => {
-  <main className="relative">
-    Nav
-    <section className="xl:padding-1 wide:padding-r padding-b">
-    </section>
-  </main>
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <Products />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding">
+        <Services />
+      </section>
+      <section className="padding">
+        <Offer />
+      </section>
+      <section className="padding">
+        <CustomerReview />
+      </section>
+      <section className="padding">
+        <Subscribe />
+      </section>
+      <section className="padding">
+        <Footer />
+      </section>
+    </main>
+  );
 };
 
-export default App
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,9 @@
+import Hero from './Hero.jsx'
+import Footer from './Footer.jsx'
+import Offer from './Offer.jsx'
+import CustomerReview from './CustomerReview.jsx'
+import Products from './Products.jsx'
+import Services from './Services.jsx'
+import SuperQuality from './SuperQuality.jsx'
+import Subscribe from './Subscribe.jsx'
+export { Hero, Footer, Offer, CustomerReview, Products, Services, SuperQuality, Subscribe }
